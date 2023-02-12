eaf7b4689f7b69e4665b6b813b1227cecbc2a5cb
tunahan <aslan.tuna.71@gmail.com>
2023-02-12 23:12:09 +0300

    Static File Uploaded

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+const path = require('path');
+const Post = require('./models/Post');
+
+const app = express();
+
+//connect DB
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://127.0.0.1:27017/clean-blog-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//Template Engine
+app.set('view engine', 'ejs');
+
+//Middlewares
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Routes
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  res.render('index', {
+    posts,
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu port ${port} başlatıldı.`);
+});
